611be91fbc71c12fc486b0589ddc5fdb98b7342d
toquete <toquete@rc.unesp.br>
2015-06-13 14:03:09 -0300

    Algoritmo Livro

--- main.cpp
+++ main.cpp
@@ -9,131 +9,200 @@
 
 #define NUM_PONTOS 4
 
-typedef struct aresta{
-    float y_max;
-    float x_in_ymin;
-    float mx;
-    aresta *prox;
-};
-
-typedef struct et{
-    float y_min;
-    et *prox_y;
-    aresta *prox_aresta;
-};
+typedef struct tAresta{
+  int y_max;
+  float x_in_ymin, mx;
+  struct tAresta *prox;
+} Aresta;
 
 struct pontos{
-    float x;
-    float y;
-}pontos[NUM_PONTOS];
+  int x;
+  int y;
+}points[NUM_PONTOS];
 
-et *listaET = NULL;
-aresta *listaAET = NULL;
-
-
-float x1,x2,x3,x4,y1,y2,y3,y4;
-
-void draw_pixel(int x,int y)
+void insereAresta(Aresta *lista, Aresta *aresta)
 {
-    glColor3f(0.0,1.0,1.0);
-    glPointSize(1.0);
-    glBegin(GL_POINTS);
-    glVertex2i(x,y);
-    glEnd();
-}
+  Aresta *p, *q = lista;
 
-void edgedetect(float x1,float y1,float x2,float y2,int *le,int *re)
-{
-    float temp,x,mx;
-    int i;
-
-    if(y1>y2)
-    {
-        temp=x1,x1=x2,x2=temp;
-        temp=y1,y1=y2,y2=temp;
-    }
-
-    if(y1==y2)
-        mx=x2-x1;
+  p = q->prox;
+  while (p)
+  {
+    if (aresta->x_in_ymin < p->x_in_ymin)
+      p = NULL;
     else
-        mx=(x2-x1)/(y2-y1);
-
-    x=x1;
-
-    for(i=int(y1);i<=(int)y2;i++)
     {
-        if(x<(float)le[i]) le[i]=(int)x;
-        if(x>(float)re[i]) re[i]=(int)x;
-        x+=mx;
+      q = p;
+      p = p->prox;
     }
+  }
+  aresta->prox = q->prox;
+  q->prox = aresta;
 }
 
-void scanfill(float x1,float y1,float x2,float y2,float x3,float y3,float x4,float y4)
+int proxY(int i, int cont, pontos *pts)
 {
-    int le[500],re[500],i,j;
+  int j;
 
-    for(i=0;i<500;i++)
-        le[i]=500,re[i]=0;
+  if ((i + 1) > (cont - 1))
+    j = 0;
+  else
+    j = i + 1;
+  while (pts[i].y == pts[j].y)
+    if ((j + 1) > (cont - 1))
+      j = 0;
+    else
+      j++;
+  return pts[j].y;
+}
 
-    edgedetect(x1,y1,x2,y2,le,re);
-    edgedetect(x2,y2,x3,y3,le,re);
-    edgedetect(x3,y3,x4,y4,le,re);
-    edgedetect(x4,y4,x1,y1,le,re);
+void inicializaAresta(pontos menor, pontos maior, int yComp, Aresta *aresta, Aresta *arestas[])
+{
+  aresta->mx = (float) (maior.x - menor.x) / (maior.y - menor.y);
+  aresta->x_in_ymin = menor.x;
+  if (maior.y < yComp)
+    aresta->y_max = maior.y  - 1;
+  else
+    aresta->y_max = maior.y;
+  insereAresta(arestas[menor.y], aresta);
+}
 
-    for(j=0;j<500;j++)
+void montaET(int cont, pontos *pts, Aresta *arestas[])
+{
+  Aresta *aresta;
+  pontos P1, P2;
+  int yAnt = pts[cont - 2].y;
+
+  P1.x = pts[cont - 1].x;
+  P1.y = pts[cont - 1].y;
+  for(int i = 0; i < cont; cont++)
+  {
+    P2 = pts[i];
+    if (P1.y != P2.y)
     {
-        if(le[j]<=re[j])
-            for(i=le[j];i<re[j];i++)
-                draw_pixel(i,j);
+      aresta = (Aresta *)malloc(sizeof (Aresta));
+      if (P1.y < P2.y)
+        inicializaAresta(P1, P2, proxY(i, cont, pts), aresta, arestas);
+      else
+        inicializaAresta(P2, P1, yAnt, aresta, arestas);
     }
+    yAnt = P1.y;
+    P1 = P2;
+  }
 }
 
-
-void display()
+void montaAET(int varredura, Aresta *ativa, Aresta *arestas[])
 {
-    x1=250.0;y1=200.0;x2=150.0;y2=300.0;x3=250.0;
-    y3=400.0;x4=350.0;y4=300.0;
-    glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(0.0,0.0,1.0);
-    glBegin(GL_LINE_LOOP);
-    glVertex2f(x1,y1);
-    glVertex2f(x2,y2);
-    glVertex2f(x3,y3);
-    glVertex2f(x4,y4);
-    glEnd();
+  Aresta *p, *q;
 
-    scanfill(x1,y1,x2,y2,x3,y3,x4,y4);
-
-    glFlush();
+  p = arestas[varredura]->prox;
+  while(p)
+  {
+    q = p->prox;
+    insereAresta(ativa, p);
+    p = q;
+  }
 }
 
-
-void init()
+void preencheLinha(int varredura, Aresta *ativa)
 {
-    glClearColor(1.0,1.0,1.0,1.0);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0.0,499.0,0.0,499.0);
+  Aresta *p1, *p2;
+
+  glClear(GL_COLOR_BUFFER_BIT);
+  glBegin(GL_POINTS);
+
+  p1 = ativa->prox;
+  while(p1)
+  {
+    p2 = p1->prox;
+    for (int i = p1->x_in_ymin; i < p2->x_in_ymin; i++)
+      glVertex2i((int)i, varredura);
+    p1 = p2->prox;
+  }
+  glEnd();
+  glFlush();
 }
 
-/*void ordenaPontos()
+void deleta(Aresta *q)
 {
-    int a, j, passo, troca = 1, trocas = 0;
+  Aresta *p = q->prox;
 
-	for (passo = 0; passo < tam-1 && troca; passo++)
-	{
-      troca = 0;
-      for (j = 0; j < tam-passo-1; j++)
-        if (pontos[j].y > pontos[j+1].y)
-        {
-            troca = 1;
-            a = aux[j];
-            aux[j] = aux[j+1];
-            aux[j+1] = a;
-            trocas++;
-        }
-	}
-}*/
+  q->prox = p->prox;
+  free(p);
+}
+
+void atualizaAET(int varredura, Aresta *ativa)
+{
+  Aresta *q = ativa, *p = ativa->prox;
+
+  while(p)
+  {
+    if (varredura >= p->y_max)
+    {
+      p = p->prox;
+      deleta(q);
+    }
+    else
+    {
+      p->x_in_ymin = p->x_in_ymin + p->mx;
+      q = p;
+      p = p->prox;
+    }
+  }
+}
+
+void ordenaAET(Aresta *ativa)
+{
+  Aresta *q, *p = ativa->prox;
+
+  ativa->prox = NULL;
+  while(p)
+  {
+    q = p->prox;
+    insereAresta(ativa, p);
+    p = q;
+  }
+}
+
+void scanLine(int cont, pontos *pts)
+{
+  Aresta *arestas[500], *ativa;
+
+  for (int i = 0; i < 500; i++)
+  {
+    arestas[i] = (Aresta *)malloc(sizeof (Aresta));
+    arestas[i]-> prox = NULL;
+  }
+  montaET(cont, pts, arestas);
+  ativa = (Aresta *)malloc(sizeof (Aresta));
+  ativa->prox = NULL;
+
+  for(int varredura = 0; varredura < 500; varredura++)
+  {
+    montaAET(varredura, ativa, arestas);
+    if (ativa->prox)
+    {
+      preencheLinha(varredura, ativa);
+      atualizaAET(varredura, ativa);
+      ordenaAET(ativa);
+    }
+  }
+}
+
+void inicializaOpenGL()
+{
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); //Define o modo inicial de apresentação
+  glutInitWindowPosition(0,0); // Define a posição inicial da tela a ser apresentada
+  glutInitWindowSize(500, 500); // Define o tamanho da tela a ser apresentada
+  glutCreateWindow("Scan Line");
+}
+
+void inicializaCores()
+{
+  glClearColor(1.0,1.0,1.0,0);// define a cor de fundo da janela (no caso, branco)
+  glColor3f(1.0,0.0,0.0);// define cor do traço (no caso, vermelho)
+  glPointSize(4.0);// define espessura do traço
+  gluOrtho2D(0 , 640 , 0 , 480);
+}
 
 void definePontos()
 {
@@ -142,109 +211,24 @@ void definePontos()
   {
      printf("\nPonto %d: \n", i+1);
      printf("\nx%d:  ", i+1);
-     scanf("%f", &pontos[i].x);
+     scanf("%d", &points[i].x);
      printf("\ny%d:  ", i+1);
-     scanf("%f", &pontos[i].y);
+     scanf("%d", &points[i].y);
   }
 }
 
-void montaListaET(float x1, float y1, float x2, float y2)
+void chamaScanLine()
 {
-    et *aux, *reg;
-    aresta *p, *q;
-    float y_max, x_in_ymin, mx, y_min;
-    int achou = 0;
-
-    if (y1 < y2)
-    {
-      y_min = y1;
-      x_in_ymin = x1;
-      y_max = y2;
-    }
-    else
-    {
-      y_min = y2;
-      x_in_ymin = x2;
-      y_max = y1;
-    }
-    mx = 1/((y2-y1)/(x2-x1));
-    aux = listaET;
-    for(aux; aux->prox_y != NULL; aux = aux->prox_y){
-      if (aux)
-      {
-         if (aux->y_min == y_min)
-         {
-             q = aux->prox_aresta;
-             for(q; q->prox != NULL; q = q->prox){
-               if (q->prox == NULL)
-               {
-                   p = (aresta *)malloc(sizeof(aresta));
-                   p->y_max = y_max;
-                   p->x_in_ymin = x_in_ymin;
-                   p->mx = mx;
-                   p->prox = NULL;
-                   q->prox = p;
-               }
-             }
-         }
-         else if(aux->prox_y == NULL)
-         {
-            reg = (et *)malloc(sizeof(et));
-            reg->y_min = y_min;
-            reg->prox_y = NULL;
-            p = (aresta *)malloc(sizeof(aresta));
-            p->y_max = y_max;
-            p->x_in_ymin = x_in_ymin;
-            p->mx = mx;
-            p->prox = NULL;
-            reg->prox_aresta = p;
-         }
-      }
-      else
-      {
-            reg = (et *)malloc(sizeof(et));
-            reg->y_min = y_min;
-            reg->prox_y = NULL;
-            p = (aresta *)malloc(sizeof(aresta));
-            p->y_max = y_max;
-            p->x_in_ymin = x_in_ymin;
-            p->mx = mx;
-            p->prox = NULL;
-            reg->prox_aresta = p;
-      }
-    }
-}
-
-void montaAET()
-{
-    aresta *q, *novo;
-    et *p;
-    q = listaAET;
-    p = listaET;
-    novo = p->prox_aresta;
-    for(p; p->prox_y != NULL; p = p->prox_y)
-    {
-
-    }
+  scanLine(NUM_PONTOS, points);
 }
 
 int main(int argc, char **argv)
 {
     glutInit(&argc,argv);
     definePontos();
-    for(int i = 0; i < NUM_PONTOS; i++){
-        if (i == (NUM_PONTOS - 1))
-          montaListaET(pontos[i].x, pontos[i].y, pontos[0].x, pontos[0].y)
-        else
-          montaListaET(pontos[i].x, pontos[i].y, pontos[i+1].x, pontos[i+1].y);
-    }
-    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-    glutInitWindowSize(500,500);
-
-    glutCreateWindow("scanline");
-    glutDisplayFunc(display);
-
-    init();
+    inicializaOpenGL();
+    inicializaCores();
+    glutDisplayFunc(chamaScanLine);
     glutMainLoop();
 
     return 0;
